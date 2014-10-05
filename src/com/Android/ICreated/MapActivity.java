8d9515459616a725ba4557c51e849f123590bb45
Mikhail Tashkinov <mikhail.tashkinov@medm.com>
2014-10-05 18:06:58 +0400

    Our super app!

---
+++ src/com/Android/ICreated/MapActivity.java
@@ -0,0 +1,44 @@
+package com.Android.ICreated;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapActivity extends FragmentActivity implements GoogleMap.OnMapClickListener
+{
+    SupportMapFragment mapFragment;
+    GoogleMap map;
+    UiSettings uiSettings;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        getActionBar().hide();
+
+        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        map = mapFragment.getMap();
+        if (map == null)
+        {
+            finish();
+            return;
+        }
+        map.setOnMapClickListener(this);
+        uiSettings = map.getUiSettings();
+        uiSettings.setCompassEnabled(false);
+    }
+
+    @Override
+    public void onMapClick(LatLng latLng)
+    {
+        map.addMarker(new MarkerOptions()
+            .position(latLng)
+            .title("Вечеринка")
+        );
+    }
+}
---
+++ src/com/Android/ICreated/Storage.java
@@ -0,0 +1,16 @@
+package com.Android.ICreated;
+
+import android.app.Application;
+import android.os.Bundle;
+
+/**
+ * Created by Mikhail on 04.10.2014.
+ */
+public final class Storage extends Application
+{
+    @Override
+    public void onCreate()
+    {
+        super.onCreate();
+    }
+}
