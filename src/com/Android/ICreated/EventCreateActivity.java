fb597acdc40c0bfee7a9f0384b0ac02172cb3165
Nik <borovikow.nikita@gmail.com>
2014-10-06 15:12:59 +0400

    Logs deleted

--- src/com/Android/ICreated/EventCreateActivity.java
+++ src/com/Android/ICreated/EventCreateActivity.java
@@ -2,8 +2,11 @@ package com.Android.ICreated;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -12,11 +15,14 @@ import java.util.Calendar;
 /**
  * Created by Ник on 05.10.2014.
  */
-public class EventCreateActivity extends Activity
+public class EventCreateActivity extends Activity implements TextWatcher
 {
     Storage storage;
     EditText title_text;
     EditText description_text;
+    Button save_button;
+    String title;
+    String description;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -27,26 +33,43 @@ public class EventCreateActivity extends Activity
         storage = (Storage)getApplication();
         title_text = (EditText) findViewById(R.id.title_text);
         description_text = (EditText) findViewById(R.id.description_text);
+        save_button = (Button) findViewById(R.id.save_button);
+        title_text.addTextChangedListener(this);
+        description_text.addTextChangedListener(this);
     }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {}
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+    @Override
+    public void afterTextChanged(Editable s)
+    {
+        String title = title_text.getText().toString();
+        String description = description_text.getText().toString();
+        if (title.matches("") || description.matches(("")))
+        {
+            save_button.setEnabled(false);
+        }
+        else
+        {
+            save_button.setEnabled(true);
+        }
+    }
+
 
     public void finish(View view)
     {
         finish();
     }
+
+
     public void saving(View view)
     {
         String title = title_text.getText().toString();
         String description = description_text.getText().toString();
-        if (title.matches(""))
-        {
-            Toast.makeText(this, "Please, enter a title", Toast.LENGTH_SHORT).show();
-            return;
-        }
-        if (description.matches(""))
-        {
-            Toast.makeText(this, "Please, enter a description", Toast.LENGTH_SHORT).show();
-            return;
-        }
         Event event = new Event(Calendar.getInstance(), storage.getCurLatLng(), title, description);
         storage.addEvent(event);
         finish();
