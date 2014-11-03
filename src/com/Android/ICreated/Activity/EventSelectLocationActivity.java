9d4f93a21b74667538a211dff679ce3cd22effaa
Nik <borovikow.nikita@gmail.com>
2014-11-03 17:03:55 +0300

    Marker fix

--- src/com/Android/ICreated/Activity/EventSelectLocationActivity.java
+++ src/com/Android/ICreated/Activity/EventSelectLocationActivity.java
@@ -12,15 +12,15 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ListView;
 import com.Android.ICreated.DrawerAdapter;
 import com.Android.ICreated.R;
+import com.Android.ICreated.Storage;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
@@ -29,6 +29,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class EventSelectLocationActivity extends FragmentActivity implements GoogleMap.OnMapClickListener
 {
     SupportMapFragment mapFragment;
+    Storage storage;
     GoogleMap map;
     UiSettings uiSettings;
     String[] drawerTitles;
@@ -45,9 +46,11 @@ public class EventSelectLocationActivity extends FragmentActivity implements Goo
         setContentView(R.layout.location_map);
         ActionBar actionBar = getActionBar();
         actionBar.setTitle(R.string.events);
+        storage = (Storage)getApplication();
 
         mapInit();
         drawerInit();
+
     }
 
     @Override
@@ -56,8 +59,17 @@ public class EventSelectLocationActivity extends FragmentActivity implements Goo
         barMenu = menu;
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.create_action_bar, menu);
-        menu.findItem(R.id.btnSaveEvent).setEnabled(false);
-        barMenu.findItem(R.id.btnSaveEvent).setIcon(R.drawable.complete_grey);
+        if (storage.getCurLatLng() != null)
+        {
+            map.addMarker(new MarkerOptions().position(storage.getCurLatLng()));
+            barMenu.findItem(R.id.btnSaveEvent).setEnabled(true);
+            barMenu.findItem(R.id.btnSaveEvent).setIcon(R.drawable.complete);
+        }
+        else
+        {
+            barMenu.findItem(R.id.btnSaveEvent).setEnabled(false);
+            barMenu.findItem(R.id.btnSaveEvent).setIcon(R.drawable.complete_grey);
+        }
         return super.onCreateOptionsMenu(menu);
     }
 
@@ -67,7 +79,6 @@ public class EventSelectLocationActivity extends FragmentActivity implements Goo
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawerList = (ListView) findViewById(R.id.left_drawer);
         String[] icons = getResources().getStringArray(R.array.drawer_icons);
-        Log.d("Logs", "" + drawerList);
         drawerList.setAdapter(new DrawerAdapter(this, R.layout.drawer_list_elem, R.id.tvTitle, R.id.tvIcon, drawerTitles, icons, getResources().getString(R.string.menu_font)));
 
         drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.drawable.bar_icon, R.string.app_name, R.string.events)
@@ -128,8 +139,7 @@ public class EventSelectLocationActivity extends FragmentActivity implements Goo
             map.clear();
             latitude = latLng.latitude;
             longitude = latLng.longitude;
-            Marker marker = map.addMarker(new MarkerOptions().position(latLng));
-            marker.showInfoWindow();
+            map.addMarker(new MarkerOptions().position(latLng));
             barMenu.findItem(R.id.btnSaveEvent).setEnabled(true);
             barMenu.findItem(R.id.btnSaveEvent).setIcon(R.drawable.complete);
         }
