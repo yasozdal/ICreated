56cd651316c96999741325b0dc424e9fe5a949b8
Philipp <phil.dolgolev@gmail.com>
2015-05-10 15:43:39 +0300

    Tests for EventCreateActivity complete

--- app/src/androidTest/java/com/Android/ICreated/tests/EventCreateActivityTest.java
+++ app/src/androidTest/java/com/Android/ICreated/tests/EventCreateActivityTest.java
@@ -1,7 +1,10 @@
 package com.Android.ICreated.tests;
 
+import android.app.Activity;
+import android.content.res.TypedArray;
 import android.test.ActivityInstrumentationTestCase2;
 import android.test.suitebuilder.annotation.LargeTest;
+import android.test.suitebuilder.annotation.MediumTest;
 import android.test.suitebuilder.annotation.SmallTest;
 
 import com.Android.ICreated.Activity.eventCreate.EventCreateActivity;
@@ -10,7 +13,14 @@ import com.Android.ICreated.actions.MyActions;
 import com.Android.ICreated.matchers.ClassNameMatcher;
 import com.google.android.apps.common.testing.ui.espresso.Espresso;
 import com.google.android.apps.common.testing.ui.espresso.action.ViewActions;
+import com.google.android.apps.common.testing.ui.espresso.assertion.ViewAssertions;
 import com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers;
+
+import static org.hamcrest.Matchers.not;
+import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withId;
+import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withText;
+import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withClassName;
+import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withContentDescription;
 
 
 /**
@@ -19,36 +29,75 @@ import com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers;
 @LargeTest
 public class EventCreateActivityTest extends ActivityInstrumentationTestCase2<EventCreateActivity> {
 
+    private Activity eventCreateActivity;
+
     public EventCreateActivityTest() {super(EventCreateActivity.class);}
 
 
     @Override
     public void setUp() throws Exception {
         super.setUp();
-        getActivity();
-    }
-
-
-    @SmallTest
-    public void testTextViewDescription() {
-        Espresso.onView(ViewMatchers.withId(R.id.etDescription)).perform(ViewActions.typeText("Hello"));
+        eventCreateActivity = getActivity();
     }
 
     @LargeTest
-    public void testTextViewDescription2() {
-        Espresso.onView(ViewMatchers.withId(R.id.etDescription)).perform(ViewActions.typeText("My Cool Event"));
+    public void testButtonsHaveRightCondition() {
 
+        Espresso.onView(withId(R.id.btnSaveEvent)).check(ViewAssertions.matches(not(ViewMatchers.isEnabled())));
+        Espresso.onView(withId(R.id.btnCategory)).check(ViewAssertions.matches(ViewMatchers.isEnabled()));
+        Espresso.onView(withId(R.id.btnLocation)).check(ViewAssertions.matches(ViewMatchers.isEnabled()));
+        Espresso.onView(withId(R.id.btnLock)).check(ViewAssertions.matches(ViewMatchers.isEnabled()));
+        Espresso.onView(withId(R.id.btnDate)).check(ViewAssertions.matches(ViewMatchers.isEnabled()));
+        Espresso.onView(withId(R.id.btnPhoto)).check(ViewAssertions.matches(ViewMatchers.isEnabled()));
+
+        Espresso.onView(withId(R.id.btnSaveEvent)).check(ViewAssertions.matches(ViewMatchers.isClickable()));
+        Espresso.onView(withId(R.id.btnCategory)).check(ViewAssertions.matches(ViewMatchers.isClickable()));
+        Espresso.onView(withId(R.id.btnLocation)).check(ViewAssertions.matches(ViewMatchers.isClickable()));
+        Espresso.onView(withId(R.id.btnLock)).check(ViewAssertions.matches(ViewMatchers.isClickable()));
+        Espresso.onView(withId(R.id.btnDate)).check(ViewAssertions.matches(ViewMatchers.isClickable()));
+        Espresso.onView(withId(R.id.btnPhoto)).check(ViewAssertions.matches(ViewMatchers.isClickable()));
+    }
+
+    @LargeTest
+    public void testSetDateTime() {
         Espresso.onView(ViewMatchers.withId(R.id.btnDate)).perform(ViewActions.click());
         Espresso.onView(ViewMatchers.withClassName(ClassNameMatcher.DatePicker())).perform(MyActions.setDate(10, 10, 2016));
         Espresso.onView(ViewMatchers.withText(R.string.set_date)).perform(ViewActions.click());
-        Espresso.onView(ViewMatchers.withText(R.string.set_date)).perform(ViewActions.click());
+    }
 
-        Espresso.onView(ViewMatchers.withId(R.id.btnLocation)).perform(ViewActions.click());
-        Espresso.onView(ViewMatchers.withContentDescription("Карта Google")).perform(ViewActions.click());
+    @LargeTest
+    public void testSetConcreteCategory() {
+        TypedArray cat = eventCreateActivity.getResources().obtainTypedArray(R.array.categories);
+        Espresso.onView(ViewMatchers.withId(R.id.btnCategory)).perform(ViewActions.click());
+        Espresso.onView(ViewMatchers.withText(cat.getString(0))).perform(ViewActions.click());
+        Espresso.onView(ViewMatchers.withText(R.string.ok)).perform(ViewActions.click());
+    }
+    @LargeTest
+    public void testSetCategoryWithoutChoosingConcrete() {
+        Espresso.onView(ViewMatchers.withId(R.id.btnCategory)).perform(ViewActions.click());
+        Espresso.onView(ViewMatchers.withText(R.string.ok)).perform(ViewActions.click());
+    }
 
-        Espresso.onView(ViewMatchers.withId(R.id.btnSaveEvent)).perform(ViewActions.click());
 
-        Espresso.onView(ViewMatchers.withId(R.id.btnSaveEvent)).perform(ViewActions.click());
+    @LargeTest
+    public void testFullCreatingEvent() {
+        Espresso.onView(withId(R.id.btnSaveEvent)).check(ViewAssertions.matches(not(ViewMatchers.isEnabled())));
+        Espresso.onView(withId(R.id.etDescription)).perform(ViewActions.typeText("My Cool Event"));
+
+        Espresso.onView(withId(R.id.btnSaveEvent)).check(ViewAssertions.matches(not(ViewMatchers.isEnabled())));
+        Espresso.onView(withId(R.id.btnDate)).perform(ViewActions.click());
+        Espresso.onView(withClassName(ClassNameMatcher.DatePicker())).perform(MyActions.setDate(10, 10, 2016));
+        Espresso.onView(withText(R.string.set_date)).perform(ViewActions.click());
+        Espresso.onView(withText(R.string.set_time)).perform(ViewActions.click());
+
+        Espresso.onView(withId(R.id.btnSaveEvent)).check(ViewAssertions.matches(not(ViewMatchers.isEnabled())));
+
+        Espresso.onView(withId(R.id.btnLocation)).perform(ViewActions.click());
+        Espresso.onView(withContentDescription("Карта Google")).perform(ViewActions.click());
+
+        Espresso.onView(withId(R.id.btnSaveEvent)).perform(ViewActions.click());
+
+        Espresso.onView(withId(R.id.btnSaveEvent)).perform(ViewActions.click());
 
     }
 
